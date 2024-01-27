ad7b3579fd990819c6f9a8bc6c0ec036632a31e0
anarakinson <anarakinson@gmail.com>
2024-01-27 14:26:30 +0300

    new version

--- video2audio.py
+++ video2audio.py
@@ -4,9 +4,8 @@ from moviepy.editor import *
 
 for i in range(0, 19):
     try:
-        video = VideoFileClip(f"./data/phonk/phonk_{i}.mp4")
-        video.audio.write_audiofile(f"./data/phonk_mp3/phonk_{i}.mp3")
+        video = VideoFileClip(f"./data/prodigy/prodigy_{i}.mp4")
+        video.audio.write_audiofile(f"./data/prodigy_mp3/prodigy_{i}.mp3")
     except:
         pass
 
-"metalcore_mp3"
